741f4b20b8005b8324fb10e08a249ff8ceb82d5e
unknown <ronaldobr2003@gmail.com>
2023-10-03 17:28:45 -0300

    Arruma erro

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const controllerHome = require("./src/controllers/controllerHome");
+const controllerData = require("./src/controllers/controllerData");
 
 const routes = express.Router();
 
