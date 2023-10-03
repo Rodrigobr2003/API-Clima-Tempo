be3705943f41b7450f7e8d6a098306ecd944d403
unknown <ronaldobr2003@gmail.com>
2023-10-03 14:31:47 -0300

    Carrega API clientside

--- src/controllers/controllerHome.js
+++ src/controllers/controllerHome.js
@@ -1,3 +1,17 @@
+import fetch from "node-fetch";
+
 exports.paginaHome = (req, res) => {
-  res.render("index");
+  const apiKey = process.env.APIKEY;
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      res.render("index", { weatherData: data }); // Passa os dados para o template
+    })
+    .catch((error) => {
+      console.error("Erro ao obter dados meteorológicos:", error);
+      res.status(500).json({ error: "Erro ao obter dados meteorológicos" });
+    });
 };
