b6c92ba17ce06b1aeb3fbdeb2a67b041ef6b78ec
unknown <ronaldobr2003@gmail.com>
2023-10-04 15:23:29 -0300

    Atualiza API

--- src/controllers/controllerData.js
+++ src/controllers/controllerData.js
@@ -1,9 +1,13 @@
 const axios = require("axios");
+require("dotenv").config();
 
 exports.sendData = (req, res) => {
+  const apiKey = process.env.APIKEY;
+
   axios
     .get(
-      `https://api.open-meteo.com/v1/forecast?latitude=-23.54&longitude=-46.63&hourly=temperature_2m`
+      //-23.54     -46.63   -> sp capital
+      `https://api.openweathermap.org/data/2.5/weather?lat=-23.54&lon=-46.63&appid=${apiKey}`
     )
     .then((response) => {
       res.send(response.data);
